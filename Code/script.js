e0dd7ecad314323e448fb447a4fa12071292703d
anastasia654Boo <anastasia.melnyk654@gmail.com>
2025-10-29 12:00:24 +0100

    moving + boundaries
    added randomXPosition
    and boundaries for moving and relocating

--- Code/script.js
+++ Code/script.js
@@ -8,7 +8,8 @@ import * as Util from "./util.js";
 // variables
 
 let x = 0.5;
-// first position with x
+let size = 100;
+let relSize = size / window.innerWidth;
 
 let leftKeys = ['KeyQ', 'KeyW', 'KeyE', 'KeyR','KeyT','KeyY',
              'KeyA','KeyS','KeyD','KeyF','KeyG','KeyH',
@@ -18,19 +19,45 @@ let rightKeys = ['KeyU','KeyI','KeyO','KeyP', 'BracketLeft','BracketRight',
             'KeyJ','KeyK','KeyL','Semicolon','Quote','Backslash',
           'KeyN','KeyM','Comma','Period','Slash'];
 //array of keys to go right
+let allKeys = leftKeys + rightKeys;
+//array of all keys for random relocation
 
 
 
 
 
-// functions
 function moveLeft() {
-  x -= 0.01;
+  const min = 0.0 + (relSize / 2);
+  if (x > min) {
+    x -= 0.01;
+  }
+  
 }
 
 function moveRight() {
+  const max = 1.0 - (relSize / 2);
+  if (x < max) {
   x += 0.01;
+  }
 }
+
+function randomXPosition() {
+  //x = Math.random();
+  
+  const min = 0.0 + (relSize / 2);
+  const max = 1.0 - (relSize / 2);
+  x = Math.random() * (max - min) + min;
+        /* can't use [size / 2] because size is 100 
+        and the screen in from 0 to 1
+
+        relative Size counts the space Hero takes on the screen (%)
+        so we could change the screen width
+        
+        and than math.random() with correct bounds
+        */
+}
+
+
 
 
 
@@ -39,6 +66,7 @@ function moveRight() {
 // Code that runs over and over again
 function loop() {
   Util.setPosition (x, 0.75);
+  Util.setSize (100);
   window.requestAnimationFrame(loop);
 }
 
@@ -55,7 +83,8 @@ function setup() {
       {moveLeft()};
     if(rightKeys.includes(event.code) && event.repeat)
       {moveRight()};
-      
+    if(allKeys.includes(event.code) && !event.repeat)
+      {randomXPosition()}
   })
 
 
